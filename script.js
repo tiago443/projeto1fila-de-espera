82db9c45f8aaa617219709e77c60b18070d5ec65
tiago sousa <francisco.barbosa482@aluno.ce.gov.br>
2024-12-09 16:38:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-if (opcao === "1") {
-    const nomes = prompt("Digite os nomes dos novos pacientes, separados por virgula:")
-    if (nomes) {
-        const novosPacientes = nomes.split(",")
-        fila = fila.concat(novosPacientes)
-        alert("Pacientes adicionados a fila.")
-    }
-} else if (opcao === "2") {
-    if (fila[0] !== undefined) {
-        const paciente = fila[0]
-        fila.splice(0, 1)
-        alert(`Consultando paciente: ${paciente}`)
-    } else {
-        alert("Nao ha pacientes na fila.")
-    }
-}
-while (opcao !== "3")
+let queue = []
+let options
 
-alert("Saindo...")
+do {
+    let listaFila = "Fila:\n"
+    if (queue.length === 0) {
+        listaFila += "Nenhum paciente na fila."
+    } else {
+        for (let i = 0; i < queue.length; i++) {
+            listaFila += `${i + 1}º - ${queue[i]}\n`
+        }
+    }
+
+    let menu = listaFila + "\n\nEscolha uma opção:\n" +
+               "1 - Novo Paciente\n" +
+               "2 - Consultar Paciente\n" +
+               "4 - Encerrar"
+
+    options = prompt(menu)
+
+    if (options === "1") {
+        let nomePaciente = prompt("Digite o nome do paciente:")
+        if (nomePaciente) {
+            queue.push(nomePaciente)
+            alert(nomePaciente + " adicionado à fila.")
+        } else {
+            alert("Nome não pode ser vazio.")
+        }
+    } else if (options === "2") {
+        if (queue.length > 0) {
+            alert("Paciente consultado: " + queue.shift())
+        } else {
+            alert("Nenhum paciente na fila.")
+        }
+    }
+} while (options !== "4")
+
+alert("Sistema encerrado.")
